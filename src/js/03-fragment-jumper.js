4ac8653da268fff1866f9eb780999de75bbd7fbc
Alexander Eschenbrenner <alexander.eschenbrenner@plentymarkets.com>
2020-11-10 15:43:37 +0100

    removed semi

--- src/js/03-fragment-jumper.js
+++ src/js/03-fragment-jumper.js
@@ -24,7 +24,7 @@
     var diff = 108
     var navs = document.getElementsByTagName('nav')
     if (navs !== null && navs.length !== 0 && navs[0] != null) {
-      diff = navs[0].clientHeight;
+      diff = navs[0].clientHeight
     }
     window.scrollTo(0, computePosition(this, 0) - diff)
   }
