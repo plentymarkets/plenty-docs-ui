ee2184178c6b1b749f9ba3b06f89aa515445c417
Petrisor Frincu <petrisor.frincu@plentymarkets.com>
2021-12-15 12:20:13 +0200

    Update for code review

--- src/js/10-custom-lang-picker.js
+++ src/js/10-custom-lang-picker.js
@@ -1,94 +1,57 @@
-  let nextLang = 1;
-  let nextLangTxt = 'en-gb';
-  let currentLangLabel = 'DE';
-  let nextLangLabel = 'EN';
-  if (window.location.pathname.includes('/en-gb/')) {
-    nextLang = 0;
-    nextLangTxt = 'de-de';
-    currentLangLabel = 'EN';
-    nextLangLabel = 'DE';
-  }
-  const thePath = window.location.pathname.replace('.html', '');
-  let theUrl = '/' + nextLangTxt;
-
-  $('#langToggle').html(currentLangLabel);
-  $('#langSelect').html(nextLangLabel);
-
-  $('#languagePath').click(function(){
-    if (!fileExists('mapper.json')) {
-      window.location.href =  theUrl;
-    }
-    $.getJSON('mapper.json')
-      .done((data) => {
-        const langObj = getObjects(data,'url', thePath);
-        // Fallback solution - go to nextlang homepage
-        if (!langObj[0] || !langObj[0].languageID){
-          window.location.href =  theUrl;
-        }
-        const typic = getObjects(data,'languageID', langObj[0].languageID);
-        // Fallback solution - go to nextlang homepage
-        if (!typic[nextLang] || !typic[nextLang].url){
-          window.location.href =  theUrl;
-        }
-
-        if (typic[nextLang].url.includes('/en/')){
-          theUrl = typic[nextLang].url.replace('/en/','/en-gb/');
-        }else{
-          theUrl = '/de-de/' + typic[nextLang].url;
-        }
-
-        window.location.href =  theUrl + '.html';
-      })
-
-  })
-
-function fileExists(url) {
-  const http = new XMLHttpRequest();
-  http.open('HEAD', url, false);
-  http.send();
-  return http.status !== 404;
+let nextLang = 1
+let nextLangTxt = 'en-gb'
+let currentLangLabel = 'DE'
+let nextLangLabel = 'EN'
+if (window.location.pathname.includes('/en-gb/')) {
+  nextLang = 0
+  nextLangTxt = 'de-de'
+  currentLangLabel = 'EN'
+  nextLangLabel = 'DE'
 }
+const thePath = window.location.pathname.replace('.html', '')
+let theUrl = '/' + nextLangTxt
 
-function getObjects(obj, key, val) {
-  var objects = [];
+$('#langToggle').html(currentLangLabel)
+$('#langSelect').html(nextLangLabel)
+
+$('#languagePath').click(function () {
+  $.getJSON('mapper.json')
+    .done((data) => {
+      const langObj = getObjects(data, 'url', thePath)
+      // Fallback solution - go to nextlang homepage
+      if (!langObj[0] || !langObj[0].languageID) {
+        window.location.href = theUrl
+      }
+      const typic = getObjects(data, 'languageID', langObj[0].languageID)
+      // Fallback solution - go to nextlang homepage
+      if (!typic[nextLang] || !typic[nextLang].url) {
+        window.location.href = theUrl
+      }
+
+      if (typic[nextLang].url.includes('/en/')) {
+        theUrl = typic[nextLang].url.replace('/en/', '/en-gb/')
+      } else {
+        theUrl = '/de-de/' + typic[nextLang].url
+      }
+
+      window.location.href = theUrl + '.html'
+    })
+})
+
+function getObjects (obj, key, val) {
+  var objects = []
   for (var i in obj) {
-    if (!obj.hasOwnProperty(i)) continue;
-    if (typeof obj[i] == 'object') {
-      objects = objects.concat(getObjects(obj[i], key, val));
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
+    if (typeof obj[i] === 'object') {
+      objects = objects.concat(getObjects(obj[i], key, val))
     } else
-    if (i == key && obj[i] == val || i == key && val == '') { //
-      objects.push(obj);
-    } else if (obj[i] == val && key == ''){
-      if (objects.lastIndexOf(obj) == -1){
-        objects.push(obj);
+    if ((i === key && obj[i] === val) || (i === key && val === '')) {
+      objects.push(obj)
+    } else if (obj[i] === val && key === '') {
+      if (objects.lastIndexOf(obj) === -1) {
+        objects.push(obj)
       }
     }
   }
-  return objects;
-}
-
-function getValues(obj, key) {
-  var objects = [];
-  for (var i in obj) {
-    if (!obj.hasOwnProperty(i)) continue;
-    if (typeof obj[i] == 'object') {
-      objects = objects.concat(getValues(obj[i], key));
-    } else if (i == key) {
-      objects.push(obj[i]);
-    }
-  }
-  return objects;
-}
-
-function getKeys(obj, val) {
-  var objects = [];
-  for (var i in obj) {
-    if (!obj.hasOwnProperty(i)) continue;
-    if (typeof obj[i] == 'object') {
-      objects = objects.concat(getKeys(obj[i], val));
-    } else if (obj[i] == val) {
-      objects.push(i);
-    }
-  }
-  return objects;
+  return objects
 }
