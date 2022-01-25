bc9f03bbbe2845f0788f7d118c05aea75009f341
Petrisor Frincu <petrisor.frincu@plentymarkets.com>
2022-01-25 14:06:16 +0200

    DCJ/224999 - FIX i18n

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -2,6 +2,6 @@
 
 module.exports = (...data) => {
   const currentLang = data[0] || 'en-gb'
-  const trans = require(process.cwd() + '/build/de-de/_/lang/' + currentLang + '.json')
+  const trans = require(process.cwd() + '/build/' + currentLang + '/_/lang/' + currentLang + '.json')
   return trans[data[1]]
 }
