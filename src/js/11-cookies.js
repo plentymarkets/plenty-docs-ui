f7a398063e51888bd096a1b90fb38d36868604fd
Sorin Ene <sorin.ene@plentymarkets.com>
2022-01-27 12:15:39 +0200

    dcj/223474 - fixed cookie.js lint errors and fixed css on dark theme

--- src/js/11-cookies.js
+++ src/js/11-cookies.js
@@ -1,90 +1,92 @@
-const name = "plenty-cookies"
+const name = 'plenty-cookies'
 
 class Cookies {
   constructor () {
     this.addListeners()
-    console.log(document.cookie.indexOf(name))
-    if (document.cookie.indexOf(name) === -1 ) {
+    if (document.cookie.indexOf(name) === -1) {
       this.toggleCookies()
     }
     this.setDefaultSettings()
   }
 
-  addListeners(){
-    let acceptAllButton = document.getElementsByClassName("cookie-accept")
-    let acceptSelectedButton = document.getElementsByClassName("cookie-accept-selection")
-    let declineButton = document.getElementsByClassName("cookie-deny")
-    let showfurtherSettings = document.getElementById("show-cookie-further-settings")
-    let hidefurtherSettings = document.getElementById("hide-cookie-further-settings")
+  addListeners () {
+    const acceptAllButton = document.getElementsByClassName('cookie-accept')
+    const acceptSelectedButton = document.getElementsByClassName('cookie-accept-selection')
+    const declineButton = document.getElementsByClassName('cookie-deny')
+    const showfurtherSettings = document.getElementById('show-cookie-further-settings')
+    const hidefurtherSettings = document.getElementById('hide-cookie-further-settings')
 
-    acceptAllButton[0].addEventListener("click", () => { this.setCookie(true, true, true, true) })
-    acceptSelectedButton[0].addEventListener("click", () => { this.setCookie(this.consent, this.statistics, this.external, this.functional) })
-    declineButton[0].addEventListener("click", () => { this.setCookie(false, false, false, false) })
-    showfurtherSettings.addEventListener("click", () => { this.toggleExtraSettings('show') })
-    hidefurtherSettings.addEventListener("click", () => { this.toggleExtraSettings('hide') })
+    acceptAllButton[0].addEventListener('click', () => { this.setCookie(true, true, true, true) })
+    acceptSelectedButton[0].addEventListener('click', () => {
+      this.setCookie(this.consent, this.statistics, this.external, this.functional)
+    })
+    declineButton[0].addEventListener('click', () => { this.setCookie(false, false, false, false) })
+    showfurtherSettings.addEventListener('click', () => { this.toggleExtraSettings('show') })
+    hidefurtherSettings.addEventListener('click', () => { this.toggleExtraSettings('hide') })
 
-    document.querySelectorAll(".cookie-settings").forEach(item => {
-      item.addEventListener('click', event => {
+    document.querySelectorAll('.cookie-settings').forEach((item) => {
+      item.addEventListener('click', (event) => {
         this[item.getAttribute('source')] = !this[item.getAttribute('source')]
       })
     })
   }
 
-  toggleCookies(action) {
+  toggleCookies (action) {
     let display = 'block'
-    if(action === 'hide'){
+    if (action === 'hide') {
       display = 'none'
     }
     document.getElementsByClassName('cookie-bar')[0].style.display = display
   }
 
-  toggleExtraSettings(action) {
-    let cookieSettingsDisplay = 'none';
-    let cookieFurtherSettingsDisplay = 'block';
-    if(action === "hide"){
-      cookieSettingsDisplay = 'flex';
-      cookieFurtherSettingsDisplay = 'none';
+  toggleExtraSettings (action) {
+    let cookieSettingsDisplay = 'none'
+    let cookieFurtherSettingsDisplay = 'block'
+    if (action === 'hide') {
+      cookieSettingsDisplay = 'flex'
+      cookieFurtherSettingsDisplay = 'none'
     }
-    document.getElementById('cookie-further-settings').style.display = cookieFurtherSettingsDisplay;
-    document.getElementById('cookie-settings').style.display = cookieSettingsDisplay;
+    document.getElementById('cookie-further-settings').style.display = cookieFurtherSettingsDisplay
+    document.getElementById('cookie-settings').style.display = cookieSettingsDisplay
   }
 
-  setCookie(consent, statistics, external, functional) {
+  setCookie (consent, statistics, external, functional) {
     this.consent = consent
     this.statistics = statistics
     this.external = external
     this.functional = functional
-    document.cookie = name+"= "+this.createCookie()+"; Secure"
+    document.cookie = name + '= ' + this.createCookie() + '; Secure'
     this.toggleCookies('hide')
     this.setDefaultSettings()
   }
 
-  createCookie() {
-    let cookie = {}
+  createCookie () {
+    const cookie = {}
     cookie.necessary = {}
     cookie.tracking = {}
     cookie.media = {}
     cookie.convenience = {}
-    cookie.necessary.convenienceHype = this.consent;
-    cookie.necessary.consent = this.consent;
-    cookie.necessary.session = this.consent;
-    cookie.necessary.csrf = this.consent;
-    cookie.tracking.googleAnalytics = this.statistics;
-    cookie.media.youtube = this.external;
-    cookie.convenience.languageDetection = this.functional;
-    return JSON.stringify(cookie);
+    cookie.necessary.convenienceHype = this.consent
+    cookie.necessary.consent = this.consent
+    cookie.necessary.session = this.consent
+    cookie.necessary.csrf = this.consent
+    cookie.tracking.googleAnalytics = this.statistics
+    cookie.media.youtube = this.external
+    cookie.convenience.languageDetection = this.functional
+    return JSON.stringify(cookie)
   }
 
-  setDefaultSettings() {
-    this.consent = true;
-    this.statistics = false;
-    this.external = false;
-    this.functional = false;
+  setDefaultSettings () {
+    this.consent = true
+    this.statistics = false
+    this.external = false
+    this.functional = false
   }
 }
 
 (function () {
   window.addEventListener('DOMContentLoaded', (event) => {
+    // eslint-disable-next-line no-new
     new Cookies()
-  });
+  })
 })()
