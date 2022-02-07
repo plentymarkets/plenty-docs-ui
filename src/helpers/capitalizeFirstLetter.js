634874140c4a7bae691e6a21225c6e4c182a4cfe
ksted <kevin.stederoth@plentymarkets.com>
2022-02-07 13:28:58 +0100

    fix: eslint errors

--- src/helpers/capitalizeFirstLetter.js
+++ src/helpers/capitalizeFirstLetter.js
@@ -1,7 +1,6 @@
 'use strict'
 
 module.exports = (...data) => {
-    const string = data[0]
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  
+  const string = data[0]
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
