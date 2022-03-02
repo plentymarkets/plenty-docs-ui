fab7d5c4d2903961af35d035c56bf61eb42183c3
ksted <kevin.stederoth@plentymarkets.com>
2022-03-02 11:52:52 +0100

    fix: console error

--- src/js/12-child-pages-list.js
+++ src/js/12-child-pages-list.js
@@ -1,6 +1,6 @@
 const currentPage = document.getElementsByClassName('is-current-page')
 let childPagesList = document.createElement('div')
-childPagesList = currentPage[0].getElementsByClassName('nav-list')[0].cloneNode(true)
-if (document.getElementById('child-pages-list')) {
+if (currentPage[0].getElementsByClassName('nav-list')[0] && document.getElementById('child-pages-list')) {
+  childPagesList = currentPage[0].getElementsByClassName('nav-list')[0].cloneNode(true)
   document.getElementById('child-pages-list').appendChild(childPagesList)
 }
