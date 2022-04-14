404be5a930163af20076b05b6792f3a107feda59
ksted <kevin.stederoth@plentymarkets.com>
2022-04-14 17:04:14 +0200

    fix: loading results message
    Displays loading msg instead of no results during fetch

---
+++ src/js/11-i18n.js
@@ -0,0 +1,11 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "translations" }] */
+
+const translations = {
+  'en-gb': {
+    results_label: 'results',
+  },
+
+  'de-de': {
+    results_label: 'Ergebnisse',
+  },
+}
