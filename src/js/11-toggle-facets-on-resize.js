9095fdf194197484567a43226d0d2aa552fe9719
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-01-27 10:35:14 +0100

    fix: resize console error

--- src/js/11-toggle-facets-on-resize.js
+++ src/js/11-toggle-facets-on-resize.js
@@ -3,11 +3,14 @@ function toggleFacetsOnResize () {
   const facetsSidebar = document.getElementById('facets-sidebar')
   const mediaQuery = window.matchMedia('(min-width: 1024px)')
 
-  if (facetsSidebar && !mediaQuery.matches) {
-    facetsSidebar.classList.add('d-none')
-  } else {
-    facetsSidebar.classList.remove('d-none')
+  if (facetsSidebar) {
+    if (mediaQuery.matches) {
+      facetsSidebar.classList.remove('d-none')
+    } else {
+      facetsSidebar.classList.add('d-none')
+    }
   }
+
   articleBody.classList.remove('mobile-no-scroll')
 }
 
