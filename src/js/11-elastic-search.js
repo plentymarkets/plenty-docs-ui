a84a0c0c7d2046bb07a2fe3b9ce7023d6a1d5f37
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-10-15 13:55:37 +0200

    Revert "chore: update knowledge domain"
    This reverts commit 119ae80ca9fd18c69b993fc3e7ecbc75af5c58ee.

--- src/js/11-elastic-search.js
+++ src/js/11-elastic-search.js
@@ -162,9 +162,7 @@ function decodeQueryParam (p) {
       const searchIcon = document.getElementById('toggle-search')
       const searchText = document.getElementById('search-input')
       const elasticSearch = new ElasticSearch()
-      const engine = window.location.href.includes('/en-gb/')
-        ? 'knowledge-en-gb-plentyone'
-        : 'knowledge-de-de-plentyone'
+      const engine = window.location.href.includes('/en-gb/') ? 'knowledge-en-gb' : 'knowledge-de-de'
       let timeout = false
       elasticSearch.setClient(engine)
       if (searchIcon && searchText) {
